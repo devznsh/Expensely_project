302dbc8bb302ba8e91dbcddeec58720d66ea4b14
Navya Yadav <yadnavya04@gmail.com>
2025-07-09 14:14:35 +0530

    Create package.js

---
+++ src/app/expensely-backend/package.js
@@ -0,0 +1,16 @@
+{
+  "name": "expensely-backend",
+  "version": "1.0.0",
+  "description": "Backend for Expensely app",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^17.1.0",
+    "express": "^5.1.0",
+    "mongoose": "^8.16.2",
+    "nodemailer": "^7.0.5"
+  }
+}
